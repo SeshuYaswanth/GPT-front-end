f84d2c16c4880e401d37e1a16efc78d7824c4b91
SeshuYaswanth <seshuyaswanthreddy01@gmail.com>
2023-06-30 17:00:42 +0530

    first commit

---
+++ src/Containers/index.js
@@ -0,0 +1,6 @@
+export { Blog } from "./blog/Blog";
+export { Features } from "./features/Features";
+export { Footer } from "./footers/Footer";
+export { Header } from "./header/Header";
+export { Possiblity } from "./possiblity/Possiblity";
+export { GPT } from "./whatGPT3/GPT";
